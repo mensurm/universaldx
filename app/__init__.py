5351bb3a95025e5bfffe3b3598f8cca53b8a265e
Mensur <mensur.mandzuka@gmail.com>
2016-01-13 19:52:38 +0100

    initialize angular app

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from flask.ext.triangle import Triangle
 
-app = Flask(__name__)
+app = Flask(__name__)  # initialize app
+Triangle(app)  # add filter for angular expresions
 
 from app import routes
---
+++ app/static/js/app/app.js
@@ -0,0 +1,5 @@
+var app = angular.module('udxApp', []);
+app.controller('getCtrl', function($scope) {
+    $scope.title= "Welcome";
+
+});
