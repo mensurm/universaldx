cd068d7b63db449f100543c8e69df7f04a65f3b2
Mensur <mensur.mandzuka@gmail.com>
2016-01-14 02:30:10 +0100

    added route that returns clinical data read from a file in JSON format

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template, jsonify, request
 from app.model import ContactForm
+import csv
 
 # APP ROUTE DEFINITIONS
 
@@ -12,7 +13,7 @@ def home():
 
 # get company info route
 @app.route('/company_info', methods=['GET'])
-def get_data():
+def company_info():
     company_details = {'name': 'Universal Diagnostics, S.L.', 'address': 'Centre of Research Technology and Innovation (CITIUS II)\
                         University of Seville. Avda. Reina Mercedes s/n', 'city': 'Seville', 'country': 'Spain', 'phone': '+34 955 420 154',
                        'email': 'contact@universaldx.com'}
@@ -21,7 +22,7 @@ def get_data():
 
 # contact us route
 @app.route('/contact_us', methods=['POST'])
-def post_data():
+def contact_us():
 
     try:
         #get posted form data
@@ -44,3 +45,34 @@ def post_data():
     except Exception:
         response = {'status': 'ERROR'}
         return jsonify(**response)
+
+
+@app.route('/clinical_data', methods=['GET'])
+def clinical_data():
+
+    samples = {}
+    with open('clinical_data_training.csv', 'r') as csvfile:
+        next(csvfile) # skip csv header line
+        rows = csv.reader(csvfile, delimiter=',')
+        for row in rows:
+            sample = {}
+            sample_id = row[0]
+            sample['condition'] = row[1]
+            sample['age'] = row[2]
+            sample['gender'] = row[3]
+            sample['race'] = row[4]
+            samples[sample_id] = sample
+
+    print samples
+
+    with open('training.csv', 'r') as csvfile:
+        next(csvfile)
+        rows = csv.reader(csvfile, delimiter=',')
+        for row in rows:
+            sample_id = row[0]
+            if sample_id in samples.keys():
+                samples[sample_id]['markers'] = [float(x) for x in row[1:]]
+
+    return jsonify(**samples)
+
+
